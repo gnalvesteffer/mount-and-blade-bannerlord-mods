cb95b3f4e5f6630b3d45486a8f15f8939509381f
ss-gnalvesteffer <gavin.alvesteffer@shipstation.com>
2020-04-14 21:58:26 -0500

    add auto upgrade troops mod

---
+++ XorberaxAutoUpgradeTroops/src/AutoUpgradeTroops/Config.cs
@@ -0,0 +1,9 @@
+using Newtonsoft.Json;
+
+namespace AutoUpgradeTroops
+{
+    internal class Config
+    {
+
+    }
+}
---
+++ XorberaxAutoUpgradeTroops/src/AutoUpgradeTroops/SubModule.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace AutoUpgradeTroops
+{
+    internal class SubModule : MBSubModuleBase
+    {
+        private static readonly string ConfigFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config.json");
+
+        public static Config Config { get; private set; }
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            LoadConfig();
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            var campaign = game.GameType as Campaign;
+            if (campaign == null)
+            {
+                return;
+            }
+            var campaignGameStarter = (CampaignGameStarter)gameStarterObject;
+            campaignGameStarter.AddBehavior(new AutoUpgradeTroopsCampaignBehavior());
+        }
+
+        private void LoadConfig()
+        {
+            if (!File.Exists(ConfigFilePath))
+            {
+                return;
+            }
+            try
+            {
+                Config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(ConfigFilePath));
+            }
+            catch
+            {
+            }
+        }
+    }
+}
